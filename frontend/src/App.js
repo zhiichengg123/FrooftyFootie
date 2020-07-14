8595336caca22bfe90a3997199449a84a68cc618
unknown <tngzhicheng@gmail.com>
2020-07-14 12:18:56 +0800

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import './App.css';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
+import {useSelector} from 'react-redux'; 
+import RegisterScreen from './screens/RegisterScreen';
+import ProductsScreen from './screens/ProductsScreen';
+import ShippingScreen from './screens/ShippingScreen';
+import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
+
+function App() {
+
+    const userSignin = useSelector(state => state.userSignin);
+    const {userInfo} = userSignin;
+
+  const openMenu = () =>{
+    document.querySelector(".sidebar").classList.add("open");
+  }
+
+  const closeMenu = () =>{
+    document.querySelector(".sidebar").classList.remove("open");
+  }
+  return (
+<BrowserRouter>
+    <div className="grid-container">
+    <header className="header">
+        <div className="brand">
+            <button onClick={openMenu}>
+                &#9776;
+            </button>
+            <Link to="/">FrooftyFootie</Link>
+        </div>
+
+        <div className="header-links">
+            <a href="cart.html"> Cart</a>
+            {
+                userInfo ? <Link to="/profile">{userInfo.name}</Link> :
+                <Link to="/signin">Sign In</Link>     
+            }
+        </div>
+    </header>
+
+    <aside className="sidebar">
+        <h3>Shopping Categories</h3>
+        <button className="sidebar-close-button" onClick={closeMenu}>x</button>
+        <ul>
+            <li>
+                <a href="index.html"> Pants </a>
+            </li>
+            <li>
+                <a href="index.html"> Shirts </a>
+            </li>
+        </ul>
+    </aside>
+
+    <main className ="main">
+        <div className="content">
+          <Route path="/placeorder" component={PlaceOrderScreen} />
+          <Route path="/payment" component={PaymentScreen} />
+          <Route path="/products" component={ProductsScreen} />
+          <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/signin" component={SigninScreen} />
+          <Route path="/product/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/" exact={true} component={HomeScreen} />
+    </div>
+    </main>
+
+    <footer className="footer">
+        All Right Reserved.
+    </footer>
+</div>
+</BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,33 @@
+export default {
+    products: [{
+        _id:'1',
+        name: 'Nike Shoe',
+        category: 'Shoe',
+        image: '/images/d1.jpg',
+        price: 289,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {   
+        _id:'2',
+        name: 'Adidas Shoe',
+        category: 'Shoe',
+        image: '/images/d1.jpg',
+        price: 489,
+        brand: 'Adidas',
+        rating: 4.5,
+        numReviews: 7,
+    },
+    {
+        _id:'3',
+        name: 'Slim Shirt',
+        category: 'Shirt',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 5,
+    }
+]
+}
