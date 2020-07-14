5d3dea28590f3821bf65e8d71c868095c421bbc2
unknown <tngzhicheng@gmail.com>
2020-07-14 12:22:27 +0800

    backend commit

---
+++ backend/data.js
@@ -0,0 +1,36 @@
+export default {
+    products: [{
+        _id:'1',
+        name: 'Nike Shoe',
+        category: 'Shoe',
+        image: '/images/d1.jpg',
+        price: 289,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10,
+        countInStock: 6,
+    },
+    {   
+        _id:'2',
+        name: 'Adidas Shoe',
+        category: 'Shoe',
+        image: '/images/d1.jpg',
+        price: 489,
+        brand: 'Adidas',
+        rating: 4.5,
+        numReviews: 7,
+        countInStock: 5,
+    },
+    {
+        _id:'3',
+        name: 'Slim Shirt',
+        category: 'Shirt',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 5,
+        countInStock: 4,
+    }
+]
+}
